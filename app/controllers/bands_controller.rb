7d2e4d89639010a060347bf6ef12ab7795a647a6
Philippe chambon <joan_krugg@live.fr>
2017-01-20 14:08:08 +0100

    bands controller & places controller

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -1,11 +1,18 @@
 class BandsController < ApplicationController
+  before_action :set_band, only: [:show, :edit, :update, :destroy]
+
   def index
+    @bands = Band.all
   end
 
   def new
+    @band = Band.new
   end
 
   def create
+    @band = Band.new(band_params)
+    @band.save
+    redirect_to band_path(@band)
   end
 
   def show
@@ -15,8 +22,22 @@ class BandsController < ApplicationController
   end
 
   def update
+    @band.update(band_params)
+    redirect_to band_path(@band)
   end
 
   def destroy
+    @band.destroy
+    redirect_to bands_path
+  end
+
+  private
+
+  def set_band
+    @band = Band.find(params[:id])
+  end
+
+  def band_params
+    params.require(:band).permit(:band_name, :rehearsal_address, :band_facebook, :genre_id, :set_time, :members_number, :band_photo, :band_mersh)
   end
 end
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,11 +1,18 @@
 class PlacesController < ApplicationController
+  before_action :set_place, only: [:show, :edit, :update, :destroy]
+
   def index
+    @places = Place.all
   end
 
   def new
+    @place = Place.new
   end
 
   def create
+    @place = Place.new(place_params)
+    @place.save
+    redirect_to place_path(@place)
   end
 
   def show
@@ -15,8 +22,23 @@ class PlacesController < ApplicationController
   end
 
   def update
+    @place.update(place_params)
+    redirect_to place_path(@place)
   end
 
   def destroy
+    @place.destroy
+    redirect_to places_path
+  end
+
+  private
+
+
+  def set_place
+    @place = Place.find(params[:id])
+  end
+
+  def place_params
+    params.require(:place).permit(:place_name, :place_address, :capacity, :place_facebook, :place_category, :place_photo, :genre_id)
   end
 end
